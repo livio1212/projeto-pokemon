0e1c3d0943124396415f2c7e3edf1c66afe6f5bd
livio1212 <liviocosta229@gmail.com>
2025-04-24 20:47:43 -0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}';
-
 const offset = 0;
 const limit = 1;
+
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}';
+
 
 fetch(url)
     .then(function(response){
