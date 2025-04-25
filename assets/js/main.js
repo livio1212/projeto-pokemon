fecf166c866ff93e762555121de3507e9bb8025c
livio1212 <liviocosta229@gmail.com>
2025-04-25 15:01:10 -0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,49 @@
 const offset = 0;
-const limit = 1;
+const limit = 20;
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 
+function convertPokemonToLi(pokemon){
+    return `
+    li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+
+                <ol class="types">
+                     <li class="type">grass</li>
+                     <li class="type">poison</li>
+
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+                alt=${pokemon.name}>
+            </div>
+            
+        </li>
+        `
+        
+}
+
+        
+const pokemonList = console.log( document.getElementById('pokemonList'));
+
+
 fetch(url)
-    .then(function(response){
-    return response.json();
-})
-.then(function(json){
-    console.log(json);
-})      
+        
+.then((response) => response.json())
+.then((json) => json.results)
+.then((pokemons) => {
+    
+   for (let i = 0; i < pokemons.length; i++) {
+     const pokemon = pokemons[i];
+        pokemonList.innerHTML += convertPokemonToLi(pokemon);
 
-.catch(function(error){
-    console.log(error);
-})
-.finally(function(){
-    console.log('requisiçao concluida');
-});
+       
+   }
 
+   
+})
+.catch((error) => console.log(error));
